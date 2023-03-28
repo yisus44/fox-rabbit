9108dc25031827abb98aa887284bc0cae3aae372
yisus44 <jesusadrian1953.1@gmail.com>
2023-03-27 18:05:53 -0600

    added differnt distance between the rabbit and fox

--- keep_bound.py
+++ keep_bound.py
@@ -1,10 +1,14 @@
 
 def keep_bound(entity, WINDOW_HEIGHT, WINDOW_WIDTH):
     if entity.left > WINDOW_WIDTH:
+        print("0 right")
         entity.right = 0
     if entity.right < 0:
+        print("0 left")
         entity.left = WINDOW_WIDTH
     if entity.top > WINDOW_HEIGHT:
-        entity.bottom = 0
+        print("0 bottom")
+        entity.top = 0
     if entity.bottom < 0:
-        entity.top = WINDOW_HEIGHT
+        print("0 top")
+        entity.bottom = WINDOW_HEIGHT
--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from keep_separate import keep_separate
 pygame.init()
 
 # Set up the game window
-WINDOW_WIDTH = 1920
-WINDOW_HEIGHT = 1080
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 800
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Fox Hunt')
 
@@ -19,19 +19,20 @@ rabbit_img = pygame.transform.scale(pygame.image.load('assets/rabbit.png'), (50,
 
 # Set up the fox and rabbit initial positions
 fox_rect = fox_img.get_rect(center=(250,250))
-rabbit_rect = rabbit_img.get_rect(center=(350 , 350 ))
+rabbit_rect = rabbit_img.get_rect(center=(350 , 250 ))
 
 # Define the game loop
 clock = pygame.time.Clock()
 running = True
-FOX_SPEED=15
-RABBIT_SPEED=15
+FOX_SPEED=50
+RABBIT_SPEED=50
 while running:
     # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
+          
             distance = pygame.math.Vector2(fox_rect.center).distance_to(pygame.math.Vector2(rabbit_rect.center))
             is_near = distance < 100
             # Move the fox with the arrow keys
@@ -52,11 +53,37 @@ while running:
                     if is_near:
                         rabbit_rect.move_ip(0, RABBIT_SPEED)
 
-            # Wrap the fox and rabbit around the screen
-            keep_bound(fox_rect, WINDOW_HEIGHT, WINDOW_WIDTH)
-            keep_bound(rabbit_rect, WINDOW_HEIGHT, WINDOW_WIDTH)
             keep_separate(rabbit_rect, fox_rect)
+
+            if rabbit_rect.left > WINDOW_WIDTH:
+                print("0 right rabbit",)
+                rabbit_rect.right = 50
+            if rabbit_rect.right < 0:
+                print("0 left rabbit",)
+                rabbit_rect.left = WINDOW_WIDTH
+            if rabbit_rect.top > WINDOW_HEIGHT:
+                print("0 bottom rabbit",)
+                rabbit_rect.top = 50
+            if rabbit_rect.bottom < 0:
+                print("0 top rabbit",)
+                rabbit_rect.bottom = WINDOW_HEIGHT
+
+
+            if fox_rect.left > WINDOW_WIDTH:
+                print("0 right, fox")
+                fox_rect.right = 0
+            if fox_rect.right < 0:
+                print("0 left, fox")
+                fox_rect.left = WINDOW_WIDTH
+            if fox_rect.top > WINDOW_HEIGHT:
+                print("0 botto, foxm")
+                fox_rect.top = 0
+            if fox_rect.bottom < 0:
+                print("0 top, ox"),
+                fox_rect.bottom = WINDOW_HEIGHT
         
+        
+        clock.tick(60)
         # Draw the game objects
         window.blit(forest_img, (0, 0))
         window.blit(fox_img, fox_rect)
